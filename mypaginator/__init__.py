58936de894d8f805f100cfa4a6041e93c6216d7f
motolies <knw1234@gmail.com>
2018-02-20 13:57:14 +0900

    Fix paging errors

--- mypaginator/__init__.py
+++ mypaginator/__init__.py
@@ -3,5 +3,5 @@ from .paginate import Pagination
 
 def pager(query, page, per_page):
     paginator = Paginator(query, per_page)
-    return Pagination(paginator, page, per_page, paginator.total_pages, paginator.page(page).object_list)
+    return Pagination(paginator, page, per_page, paginator.count, paginator.page(page).object_list)
 
